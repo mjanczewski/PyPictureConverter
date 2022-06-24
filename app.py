c5d4e95bc75afc5ed40878a79126cb6670370d2d
Marek Janczewski <mrjanczewski@gmail.com>
2022-06-24 15:24:31 +0200

    Dodanie zapisu w formacje jpg i webp

--- app.py
+++ app.py
@@ -5,9 +5,12 @@ inputFolder = './input'
 
 for filename in os.listdir(inputFolder):
     if filename.endswith('.jpg'):
+        filenameMod = filename.split('.')
+        filenameMod = str(filenameMod[0])
         print(filename)
         inputImage = Image.open(f'./input/{filename}')
-        inputImage.save(f'./output/{filename}.webp')
+        inputImage.save(f'./output/opis/{filenameMod}.webp')
+        inputImage.save(f'./output/{filenameMod}.jpg')
 
 
 # inputImage = Image.open('./input/')
